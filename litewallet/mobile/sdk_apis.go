92ad1813d18d84e749dae9dfb755a2cce906c890
wangfeiping <wangfeiping@outlook.com>
2020-09-21 10:05:54 +0800

    restores the address check api

--- litewallet/mobile/sdk_apis.go
+++ litewallet/mobile/sdk_apis.go
@@ -2,6 +2,8 @@ package litewallet
 
 import (
 	"encoding/json"
+	"fmt"
+	"regexp"
 
 	"github.com/QOSGroup/litewallet/litewallet/chains/cosmos"
 	"github.com/QOSGroup/litewallet/litewallet/types"
@@ -21,8 +23,29 @@ func CreateSeed() string {
 	return string(bytes)
 }
 
-//WalletAddressCheck for different chains
+//WalletAddressCheck to differentiate the addresses from various wallets, e.g. cosmos, ETH, qos, .etc
 func WalletAddressCheck(addr string) string {
-	output := ""
-	return output
+	//split the address with prefix, e.g. "0x", "cosmos", "address" for ETH, cosmos, qos respectively
+	//regexp for ETH address
+	ethre := regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
+
+	//regexp for cosmos address
+	cosre := regexp.MustCompile("^cosmos1[0-9a-z]{38}$")
+
+	//regexp for qos address
+	qosre := regexp.MustCompile("address1[0-9a-z]{38}$")
+
+	switch {
+	case ethre.MatchString(addr):
+		return "ETH"
+
+	case cosre.MatchString(addr):
+		return "COSMOS"
+
+	case qosre.MatchString(addr):
+		return "QOS"
+
+	default:
+		return fmt.Sprintf("None")
+	}
 }
